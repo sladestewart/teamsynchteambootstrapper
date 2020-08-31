6cfffa064571381a2927de4325b9f8218dd81da3
Slade Stewart <slade.stewart@teclallc.com>
2020-08-31 14:04:20 -0400

    changes

--- full_bootstrap/LaptopBootstrapGruntFile.js
+++ full_bootstrap/LaptopBootstrapGruntFile.js
@@ -194,7 +194,7 @@ module.exports = function(grunt) {
       function makeTsAvailableFromAnywhere() {
         showExplanation();
         if (!fs.existsSync(pathToUserLocal)) fs.mkdirSync(pathToUserLocal);
-        pathToUserLocalBin = ''; path.join(pathToUserLocal, 'bin');
+        pathToUserLocalBin = path.join(pathToUserLocal, 'bin');
         if (!fs.existsSync(pathToUserLocalBin)) fs.mkdirSync(pathToUserLocalBin);
         let pathToTsInUserLocalBin = path.join(pathToUserLocalBin, 'ts');
 
